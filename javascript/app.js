b9a24fa42206704e55ae283fbc4b28819d502341
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2016-05-20 16:19:35 -0700

    Added angular gmap api and working map for Las Vegas

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,24 @@
 /// <reference path="../typings/index.d.ts" />
 
-var app = angular.module('yelpVis', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
+var app = angular.module('yelpVis', ['ngMaterial', 'uiGmapgoogle-maps'])
+    .config(function($mdThemingProvider, uiGmapGoogleMapApiProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('red')
             .accentPalette('grey');
+
+        uiGmapGoogleMapApiProvider.configure({
+            //    key: 'your api key',
+            v: '3.20', //defaults to latest 3.X anyhow
+            libraries: 'geometry,visualization'
+        });
     });
 
 // The main controller for the application.
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, uiGmapGoogleMapApi) {
+    $scope.map = { center: { latitude: 36.1699, longitude: -115.1398 }, zoom: 12 };
+    uiGmapGoogleMapApi.then(function(maps) {
 
     });
+
+
+});
