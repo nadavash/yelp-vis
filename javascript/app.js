bbc67c938a1e1d32a6dd571614760cd7708dc49f
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2016-05-21 19:19:14 -0700

    Working demo of Bay Area radio stations!

--- javascript/app.js
+++ javascript/app.js
@@ -7,18 +7,63 @@ var app = angular.module('yelpVis', ['ngMaterial', 'uiGmapgoogle-maps'])
             .accentPalette('grey');
 
         uiGmapGoogleMapApiProvider.configure({
-            //    key: 'your api key',
-            v: '3.20', //defaults to latest 3.X anyhow
             libraries: 'geometry,visualization'
         });
     });
 
 // The main controller for the application.
 app.controller('MainController', function($scope, uiGmapGoogleMapApi) {
-    $scope.map = { center: { latitude: 36.1699, longitude: -115.1398 }, zoom: 12 };
+    $scope.map = {
+        center: { latitude: 37.76487, longitude: -122.41948 },
+        zoom: 8,
+        control: {}
+    };
+
     uiGmapGoogleMapApi.then(function(maps) {
 
+        // Create the map overlay for layering custom SVG elements on top of the
+        // map.
+        d3.json('data/stations.json', function(err, data) {
+            if (err) throw err;
+
+            var overlay = new maps.OverlayView();
+            overlay.onAdd = function() {
+                var layer = d3.select(this.getPanes().overlayLayer)
+                    .append('div')
+                    .attr('class', 'stations');
+
+                overlay.draw = function() {
+                    var projection = this.getProjection();
+                    var padding = 10;
+
+                    var marker = layer.selectAll('svg')
+                        .data(d3.entries(data))
+                        .each(transform)
+                        .enter().append('svg')
+                        .each(transform)
+                        .attr('class', 'marker');
+
+                    marker.append('circle')
+                        .attr('r', 4.5)
+                        .attr('cx', padding)
+                        .attr('cy', padding);
+
+                    marker.append('text')
+                        .attr('x', padding + 7)
+                        .attr('y', padding)
+                        .attr('dy', '.31em')
+                        .text(function(d) { return d.key; });
+
+                    function transform(d) {
+                        d = new maps.LatLng(d.value[1], d.value[0]);
+                        d = projection.fromLatLngToDivPixel(d);
+                        return d3.select(this)
+                            .style('left', (d.x - padding) + 'px')
+                            .style('top', (d.y - padding) + 'px');
+                    }
+                };
+            };
+            overlay.setMap($scope.map.control.getGMap());
+        });
     });
-
-
 });
